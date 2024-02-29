eded33e6de8e6ef9fb7200bfe86dda8952e6c553
Caleb John <johncaleb022@gmail.com>
2024-02-28 23:17:10 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
-
 // data imports
 import User from "./models/User.js";
 import Product from "./models/Product.js";
